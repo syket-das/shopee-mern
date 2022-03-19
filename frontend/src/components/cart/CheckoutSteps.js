27fa82fee228ad82623290c4303d88ca48b2a37e
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-20 05:10:20 +0530

    cart responsiveness done

--- frontend/src/components/cart/CheckoutSteps.js
+++ frontend/src/components/cart/CheckoutSteps.js
@@ -1,42 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CheckoutSteps = ({ shipping, confirmOrder, payment }) => {
-    return (
-        <div className="checkout-progress d-flex justify-content-center mt-5">
+  return (
+    <div className='d-flex justify-content-center'>
+      <div className="checkout-progress  mt-5 container ">
+        {shipping ? (
+          <Link to="shipping" className="">
+            <div className="triangle2-active my-3"></div>
+            <div className="step active-step my-3">Shipping</div>
+            <div className="triangle-active my-3"></div>
+          </Link>
+        ) : (
+          <Link to="#!" disabled>
+            <div className="triangle2-incomplete my-3"></div>
+            <div className="step incomplete my-3">Shipping</div>
+            <div className="triangle-incomplete my-3"></div>
+          </Link>
+        )}
 
-            {shipping ? <Link to='shipping' className="float-right">
-                <div className="triangle2-active"></div>
-                <div className="step active-step">Shipping</div>
-                <div className="triangle-active"></div>
-            </Link> : <Link to="#!" disabled>
-                    <div className="triangle2-incomplete"></div>
-                    <div className="step incomplete">Shipping</div>
-                    <div className="triangle-incomplete"></div>
-                </Link>}
+        {confirmOrder ? (
+          <Link to="/order/confirm" className="">
+            <div className="triangle2-active my-3"></div>
+            <div className="step active-step my-3">Confirm Order</div>
+            <div className="triangle-active my-3"></div>
+          </Link>
+        ) : (
+          <Link to="#!" disabled>
+            <div className="triangle2-incomplete my-3"></div>
+            <div className="step incomplete my-3">Confirm Order</div>
+            <div className="triangle-incomplete my-3"></div>
+          </Link>
+        )}
 
-            {confirmOrder ? <Link to='/order/confirm' className="float-right">
-                <div className="triangle2-active"></div>
-                <div className="step active-step">Confirm Order</div>
-                <div className="triangle-active"></div>
-            </Link> : <Link to="#!" disabled>
-                    <div className="triangle2-incomplete"></div>
-                    <div className="step incomplete">Confirm Order</div>
-                    <div className="triangle-incomplete"></div>
-                </Link>}
+        {payment ? (
+          <Link to="/payment" className="">
+            <div className="triangle2-active my-3"></div>
+            <div className="step active-step my-3">Payment</div>
+            <div className="triangle-active my-3"></div>
+          </Link>
+        ) : (
+          <Link to="#!" disabled>
+            <div className="triangle2-incomplete my-3"></div>
+            <div className="step incomplete my-3">Payment</div>
+            <div className="triangle-incomplete my-3"></div>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
-            {payment ? <Link to='/payment' className="float-right">
-                <div className="triangle2-active"></div>
-                <div className="step active-step">Payment</div>
-                <div className="triangle-active"></div>
-            </Link> : <Link to="#!" disabled>
-                    <div className="triangle2-incomplete"></div>
-                    <div className="step incomplete">Payment</div>
-                    <div className="triangle-incomplete"></div>
-                </Link>}
-
-        </div>
-    )
-}
-
-export default CheckoutSteps
+export default CheckoutSteps;
--- frontend/src/components/cart/Shipping.js
+++ frontend/src/components/cart/Shipping.js
@@ -35,7 +35,7 @@ const Shipping = ({ history }) => {
 
             <CheckoutSteps shipping />
 
-            <div className="row wrapper">
+            <div className="row wrapper ">
                 <div className="col-10 col-lg-5">
                     <form className="shadow-lg" onSubmit={submitHandler}>
                         <h1 className="mb-4">Shipping Info</h1>
