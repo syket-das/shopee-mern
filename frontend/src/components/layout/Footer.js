6c14f215035272b2f3f222df498c9c4dbf5518b4
syket-das <saiketsd23@gmail.com>
2022-04-01 23:22:23 +0530

    footer class - classname

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -4,32 +4,32 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <Fragment>
-      <footer class="footer-section">
-        <div class="container">
-          <div class="footer-cta pt-5 pb-5">
-            <div class="row">
-              <div class="col-xl-4 col-md-4 mb-30">
-                <div class="single-cta">
-                  <i class="fa fa-map-marker"></i>
-                  <div class="cta-text">
+      <footer className="footer-section">
+        <div className="container">
+          <div className="footer-cta pt-5 pb-5">
+            <div className="row">
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="fa fa-map-marker"></i>
+                  <div className="cta-text">
                     <h4>Find us</h4>
                     <span>Phagwara, Punjab, India</span>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-md-4 mb-30">
-                <div class="single-cta">
-                  <i class="fa fa-phone"></i>
-                  <div class="cta-text">
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="fa fa-phone"></i>
+                  <div className="cta-text">
                     <h4>Call us</h4>
                     <span>+91 8100156406</span>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-md-4 mb-30">
-                <div class="single-cta">
-                  <i class="fa fa-envelope-open"></i>
-                  <div class="cta-text">
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="fa fa-envelope-open"></i>
+                  <div className="cta-text">
                     <h4>Mail us</h4>
                     <span>saiketsd23@gmail.com</span>
                   </div>
@@ -37,43 +37,43 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div class="footer-content pt-5 pb-5">
-            <div class="row">
-              <div class="col-xl-4 col-lg-4 mb-50">
-                <div class="footer-widget">
-                  <div class="footer-logo">
+          <div className="footer-content pt-5 pb-5">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-logo">
                     <Link to="index.html">
                       <img
                         src="/images/logo.png"
-                        class="img-fluid"
+                        className="img-fluid"
                         alt="logo"
                       />
                     </Link>
                   </div>
-                  <div class="footer-text">
+                  <div className="footer-text">
                     <p>
                       Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
                       sed do eiusmod tempor incididuntut consec tetur
                       adipisicing elit,Lorem ipsum dolor sit amet.
                     </p>
                   </div>
-                  <div class="footer-social-icon">
+                  <div className="footer-social-icon">
                     <span>Follow us</span>
                     <Link to="#/">
-                      <i class="fa fa-facebook"></i>
+                      <i className="fa fa-facebook"></i>
                     </Link>
                     <Link to="#/">
-                      <i class="fa fa-twitter"></i>
+                      <i className="fa fa-twitter"></i>
                     </Link>
                     <Link to="#/">
-                      <i class="fa fa-google"></i>
+                      <i className="fa fa-google"></i>
                     </Link>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-                <div class="footer-widget">
-                  <div class="footer-widget-heading">
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
                     <h3>Useful Links</h3>
                   </div>
                   <ul>
@@ -110,22 +110,22 @@ const Footer = () => {
                   </ul>
                 </div>
               </div>
-              <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
-                <div class="footer-widget">
-                  <div class="footer-widget-heading">
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
                     <h3>Subscribe</h3>
                   </div>
-                  <div class="footer-text mb-25">
+                  <div className="footer-text mb-25">
                     <p>
                       Don’t miss to subscribe to our new feeds, kindly fill the
                       form below.
                     </p>
                   </div>
-                  <div class="subscribe-form">
+                  <div className="subscribe-form">
                     <form action="#">
                       <input type="text" placeholder="Email Address" />
                       <button>
-                        <i class="fa fa-telegram"></i>
+                        <i className="fa fa-telegram"></i>
                       </button>
                     </form>
                   </div>
@@ -134,11 +134,11 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div class="copyright-area">
-          <div class="container">
-            <div class="row">
-              <div class="col-xl-6 col-lg-6 text-center text-lg-left">
-                <div class="copyright-text">
+        <div className="copyright-area">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                <div className="copyright-text">
                   <p>
                     Copyright &copy; 2022, All Right Reserved |{' '}
                     <a
@@ -151,8 +151,8 @@ const Footer = () => {
                   </p>
                 </div>
               </div>
-              <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                <div class="footer-menu">
+              <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                <div className="footer-menu">
                   <ul>
                     <li>
                       <Link to="/">Home</Link>
