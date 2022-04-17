b9d9f9a3d7df7739e18b0565f0f955f99705a2e5
syket-das <saiketsd23@gmail.com>
2022-04-17 23:41:48 +0530

    cart link error fixed

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -8,7 +8,7 @@ import { logout } from '../../actions/userActions';
 import Search from './Search';
 
 import '../../App.css';
-import { Image } from 'react-bootstrap';
+import { Image, Navbar, Row, Col } from 'react-bootstrap';
 
 const Header = () => {
   const alert = useAlert();
@@ -24,85 +24,85 @@ const Header = () => {
 
   return (
     <Fragment>
-      <nav className="navbar row">
-        <div className="col-12 col-md-3">
-          <div className="navbar-brand">
-            <Link to="/">
-              <Image
-                src="/images/logo.png"
-              />
+      <Navbar className="w-100">
+        <Row className="d-flex align-items-center justify-content-between w-100">
+          <Col md={4}>
+            <Navbar.Brand className="navbar-brand">
+              <Link to="/">
+                <Image src="/images/logo.png" />
+              </Link>
+            </Navbar.Brand>
+          </Col>
+
+          <Col md={5} lg={6}>
+            <Route render={({ history }) => <Search history={history} />} />
+          </Col>
+
+          <Col md={2} className="">
+            <Link to="/cart" style={{ textDecoration: 'none' }}>
+              <span id="cart" className="ml-3">
+                Cart
+              </span>
+              <span className="ml-1" id="cart_count">
+                {cartItems.length}
+              </span>
             </Link>
-          </div>
-        </div>
 
-        <div className="col-12 col-md-6 mt-2 mt-md-0">
-          <Route render={({ history }) => <Search history={history} />} />
-        </div>
-
-        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <Link to="/cart" style={{ textDecoration: 'none' }}>
-            <span id="cart" className="ml-3">
-              Cart
-            </span>
-            <span className="ml-1" id="cart_count">
-              {cartItems.length}
-            </span>
-          </Link>
-
-          {user ? (
-            <div className="ml-4 dropdown d-inline">
-              <Link
-                to="#!"
-                className="btn dropdown-toggle text-white mr-4"
-                type="button"
-                id="dropDownMenuButton"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <figure className="avatar avatar-nav">
-                  <img
-                    src={user.avatar && user.avatar.url}
-                    alt={user && user.name}
-                    className="rounded-circle"
-                  />
-                </figure>
-                <span>{user && user.name}</span>
-              </Link>
-
-              <div
-                className="dropdown-menu"
-                aria-labelledby="dropDownMenuButton"
-              >
-                {user && user.role === 'admin' && (
-                  <Link className="dropdown-item" to="/dashboard">
-                    Dashboard
-                  </Link>
-                )}
-                <Link className="dropdown-item" to="/orders/me">
-                  Orders
-                </Link>
-                <Link className="dropdown-item" to="/me">
-                  Profile
-                </Link>
+            {user ? (
+              <div className="ml-4 dropdown d-inline">
                 <Link
-                  className="dropdown-item text-danger"
-                  to="/"
-                  onClick={logoutHandler}
+                  to="#!"
+                  className="btn dropdown-toggle text-white mr-4"
+                  type="button"
+                  id="dropDownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
                 >
-                  Logout
+                  <figure className="avatar avatar-nav">
+                    <img
+                      src={user.avatar && user.avatar.url}
+                      alt={user && user.name}
+                      className="rounded-circle"
+                    />
+                  </figure>
+                  <span>{user && user.name}</span>
                 </Link>
+
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropDownMenuButton"
+                >
+                  {user && user.role === 'admin' && (
+                    <Link className="dropdown-item" to="/dashboard">
+                      Dashboard
+                    </Link>
+                  )}
+                  <Link className="dropdown-item" to="/orders/me">
+                    Orders
+                  </Link>
+                  <Link className="dropdown-item" to="/me">
+                    Profile
+                  </Link>
+                  <Link
+                    className="dropdown-item text-danger"
+                    to="/"
+                    onClick={logoutHandler}
+                  >
+                    Logout
+                  </Link>
+                </div>
               </div>
-            </div>
-          ) : (
-            !loading && (
-              <Link to="/login" className="btn ml-4" id="login_btn">
-                Login
-              </Link>
-            )
-          )}
-        </div>
-      </nav>
+            ) : (
+              !loading && (
+                <Link to="/login" className="btn ml-4 my-2" id="login_btn">
+                  Login
+                </Link>
+              )
+            )}
+          </Col>
+        </Row>
+      </Navbar>
     </Fragment>
   );
 };
