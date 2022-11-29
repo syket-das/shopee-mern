9b5069ffb982af507248fd27e4386fd5b6a40633
syket-das <saiketsd23@gmail.com>
2022-11-29 11:20:06 +0530

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
-import Home from './components/Home'
-import ProductDetails from './components/product/ProductDetails'
+import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails';
 
 // Cart Imports
-import Cart from './components/cart/Cart'
-import Shipping from './components/cart/Shipping'
-import ConfirmOrder from './components/cart/ConfirmOrder'
-import Payment from './components/cart/Payment'
-import OrderSuccess from './components/cart/OrderSuccess'
+import Cart from './components/cart/Cart';
+import Shipping from './components/cart/Shipping';
+import ConfirmOrder from './components/cart/ConfirmOrder';
+import Payment from './components/cart/Payment';
+import OrderSuccess from './components/cart/OrderSuccess';
 
 // Order Imports
-import ListOrders from './components/order/ListOrders'
-import OrderDetails from './components/order/OrderDetails'
+import ListOrders from './components/order/ListOrders';
+import OrderDetails from './components/order/OrderDetails';
 
 // Auth or User imports
-import Login from './components/user/Login'
-import Register from './components/user/Register'
-import Profile from './components/user/Profile'
-import UpdateProfile from './components/user/UpdateProfile'
-import UpdatePassword from './components/user/UpdatePassword'
-import ForgotPassword from './components/user/ForgotPassword'
-import NewPassword from './components/user/NewPassword'
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import Profile from './components/user/Profile';
+import UpdateProfile from './components/user/UpdateProfile';
+import UpdatePassword from './components/user/UpdatePassword';
+import ForgotPassword from './components/user/ForgotPassword';
+import NewPassword from './components/user/NewPassword';
 
 // Admin Imports
-import Dashboard from './components/admin/Dashboard'
-import ProductsList from './components/admin/ProductsList'
-import NewProduct from './components/admin/NewProduct'
-import UpdateProduct from './components/admin/UpdateProduct'
-import OrdersList from './components/admin/OrdersList'
-import ProcessOrder from './components/admin/ProcessOrder'
-import UsersList from './components/admin/UsersList'
-import UpdateUser from './components/admin/UpdateUser'
-import ProductReviews from './components/admin/ProductReviews'
+import Dashboard from './components/admin/Dashboard';
+import ProductsList from './components/admin/ProductsList';
+import NewProduct from './components/admin/NewProduct';
+import UpdateProduct from './components/admin/UpdateProduct';
+import OrdersList from './components/admin/OrdersList';
+import ProcessOrder from './components/admin/ProcessOrder';
+import UsersList from './components/admin/UsersList';
+import UpdateUser from './components/admin/UpdateUser';
+import ProductReviews from './components/admin/ProductReviews';
 
-
-import ProtectedRoute from './components/route/ProtectedRoute'
-import { loadUser } from './actions/userActions'
-import { useSelector } from 'react-redux'
-import store from './store'
-import axios from 'axios'
+import ProtectedRoute from './components/route/ProtectedRoute';
+import { loadUser } from './actions/userActions';
+import { useSelector } from 'react-redux';
+import store from './store';
 
 // Payment
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 function App() {
-
   const [stripeApiKey, setStripeApiKey] = useState('');
 
   useEffect(() => {
-    store.dispatch(loadUser())
+    store.dispatch(loadUser());
 
-    async function getStripApiKey() {
-      const { data } = await axios.get('/api/v1/stripeapi');
+ 
 
-      setStripeApiKey(data.stripeApiKey)
-    }
+    setStripeApiKey(
+      'pk_test_51KSdQVSCo3iNZ6M6ryNOpFQfNK7PaKFGyOuleBnvsGUB4lcBMbgWcAM7jTCb8CkD9bPaoB9AliKCjZBqB9ck5BU600snkpKRTM'
+    );
+  }, []);
 
-    getStripApiKey();
-
-  }, [])
-
-  const { user, isAuthenticated, loading } = useSelector(state => state.auth)
+  const { user, isAuthenticated, loading } = useSelector((state) => state.auth);
 
   return (
     <Router>
@@ -81,11 +75,11 @@ function App() {
           <ProtectedRoute path="/shipping" component={Shipping} />
           <ProtectedRoute path="/confirm" component={ConfirmOrder} exact />
           <ProtectedRoute path="/success" component={OrderSuccess} />
-          {stripeApiKey &&
+          {stripeApiKey && (
             <Elements stripe={loadStripe(stripeApiKey)}>
               <ProtectedRoute path="/payment" component={Payment} />
             </Elements>
-          }
+          )}
 
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
@@ -93,25 +87,72 @@ function App() {
           <Route path="/password/reset/:token" component={NewPassword} exact />
           <ProtectedRoute path="/me" component={Profile} exact />
           <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
-          <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
+          <ProtectedRoute
+            path="/password/update"
+            component={UpdatePassword}
+            exact
+          />
 
           <ProtectedRoute path="/orders/me" component={ListOrders} exact />
           <ProtectedRoute path="/order/:id" component={OrderDetails} exact />
         </div>
 
-        <ProtectedRoute path="/dashboard" isAdmin={true} component={Dashboard} exact />
-        <ProtectedRoute path="/admin/products" isAdmin={true} component={ProductsList} exact />
-        <ProtectedRoute path="/admin/product" isAdmin={true} component={NewProduct} exact />
-        <ProtectedRoute path="/admin/product/:id" isAdmin={true} component={UpdateProduct} exact />
-        <ProtectedRoute path="/admin/orders" isAdmin={true} component={OrdersList} exact />
-        <ProtectedRoute path="/admin/order/:id" isAdmin={true} component={ProcessOrder} exact />
-        <ProtectedRoute path="/admin/users" isAdmin={true} component={UsersList} exact />
-        <ProtectedRoute path="/admin/user/:id" isAdmin={true} component={UpdateUser} exact />
-        <ProtectedRoute path="/admin/reviews" isAdmin={true} component={ProductReviews} exact />
+        <ProtectedRoute
+          path="/dashboard"
+          isAdmin={true}
+          component={Dashboard}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/products"
+          isAdmin={true}
+          component={ProductsList}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/product"
+          isAdmin={true}
+          component={NewProduct}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/product/:id"
+          isAdmin={true}
+          component={UpdateProduct}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/orders"
+          isAdmin={true}
+          component={OrdersList}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/order/:id"
+          isAdmin={true}
+          component={ProcessOrder}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/users"
+          isAdmin={true}
+          component={UsersList}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/user/:id"
+          isAdmin={true}
+          component={UpdateUser}
+          exact
+        />
+        <ProtectedRoute
+          path="/admin/reviews"
+          isAdmin={true}
+          component={ProductReviews}
+          exact
+        />
 
-        {!loading && (!isAuthenticated || user.role !== 'admin') && (
-          <Footer />
-        )}
+        {!loading && (!isAuthenticated || user.role !== 'admin') && <Footer />}
       </div>
     </Router>
   );
