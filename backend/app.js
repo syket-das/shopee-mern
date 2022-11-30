71609687a2ed48a74ab6263f1b782a67585e745b
syket-das <saiketsd23@gmail.com>
2022-11-30 13:39:45 +0530

    production merge

--- backend/app.js
+++ backend/app.js
@@ -32,13 +32,13 @@ app.use('/api/v1', auth);
 app.use('/api/v1', payment);
 app.use('/api/v1', order);
 
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')))
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-//     })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 // Middleware to handle errors
 app.use(errorMiddleware);
