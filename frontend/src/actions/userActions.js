b53335d900d87fa45344e3f9b0a2c7bf887f9d6a
syket-das <saiketsd23@gmail.com>
2022-11-30 10:29:29 +0530

    baseurl added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -38,6 +38,8 @@ import {
   CLEAR_ERRORS,
 } from '../constants/userConstants';
 
+const baseUrl = 'http://localhost:5000';
+
 // Login
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -50,7 +52,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      '/api/v1/login',
+      baseUrl + '/api/v1/login',
       { email, password },
       config
     );
@@ -78,7 +80,7 @@ export const register = (userData) => async (dispatch) => {
       },
     };
 
-    const { data } = await axios.post('/api/v1/register', userData, config);
+    const { data } = await axios.post(baseUrl+'/api/v1/register', userData, config);
 
     dispatch({
       type: REGISTER_USER_SUCCESS,
@@ -97,7 +99,7 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get('/api/v1/me');
+    const { data } = await axios.get(baseUrl + '/api/v1/me');
 
     dispatch({
       type: LOAD_USER_SUCCESS,
@@ -122,7 +124,11 @@ export const updateProfile = (userData) => async (dispatch) => {
       },
     };
 
-    const { data } = await axios.put('/api/v1/me/update', userData, config);
+    const { data } = await axios.put(
+      baseUrl + '/api/v1/me/update',
+      userData,
+      config
+    );
 
     dispatch({
       type: UPDATE_PROFILE_SUCCESS,
@@ -148,7 +154,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     };
 
     const { data } = await axios.put(
-      '/api/v1/password/update',
+      baseUrl + '/api/v1/password/update',
       passwords,
       config
     );
@@ -176,7 +182,11 @@ export const forgotPassword = (email) => async (dispatch) => {
       },
     };
 
-    const { data } = await axios.post('/api/v1/password/forgot', email, config);
+    const { data } = await axios.post(
+      baseUrl + '/api/v1/password/forgot',
+      email,
+      config
+    );
 
     dispatch({
       type: FORGOT_PASSWORD_SUCCESS,
@@ -202,7 +212,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     };
 
     const { data } = await axios.put(
-      `/api/v1/password/reset/${token}`,
+      baseUrl + `/api/v1/password/reset/${token}`,
       passwords,
       config
     );
@@ -222,7 +232,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
 // Logout user
 export const logout = () => async (dispatch) => {
   try {
-    await axios.get('/api/v1/logout');
+    await axios.get(baseUrl + '/api/v1/logout');
 
     dispatch({
       type: LOGOUT_SUCCESS,
@@ -240,7 +250,7 @@ export const allUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST });
 
-    const { data } = await axios.get('/api/v1/admin/users');
+    const { data } = await axios.get(baseUrl + '/api/v1/admin/users');
 
     dispatch({
       type: ALL_USERS_SUCCESS,
@@ -266,7 +276,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     };
 
     const { data } = await axios.put(
-      `/api/v1/admin/user/${id}`,
+      baseUrl + `/api/v1/admin/user/${id}`,
       userData,
       config
     );
@@ -288,7 +298,7 @@ export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.get(baseUrl + `/api/v1/admin/user/${id}`);
 
     dispatch({
       type: USER_DETAILS_SUCCESS,
@@ -307,7 +317,7 @@ export const deleteUser = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_USER_REQUEST });
 
-    const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.delete(baseUrl + `/api/v1/admin/user/${id}`);
 
     dispatch({
       type: DELETE_USER_SUCCESS,
