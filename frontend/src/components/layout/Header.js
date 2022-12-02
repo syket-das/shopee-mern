61a7ccd272e58d5dffb2b03720c1dd7ab0b6c06e
syket-das <saiketsd23@gmail.com>
2022-12-03 00:00:20 +0530

    header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -8,7 +8,7 @@ import { logout } from '../../actions/userActions';
 import Search from './Search';
 
 import '../../App.css';
-import { Image, Navbar, Row, Col } from 'react-bootstrap';
+import { Image, Navbar, Row, Col, Figure } from 'react-bootstrap';
 
 const Header = () => {
   const alert = useAlert();
@@ -22,25 +22,38 @@ const Header = () => {
     alert.success('Logged out successfully.');
   };
 
-
-
   return (
     <Fragment>
       <Navbar className="w-100">
         <Row className="d-flex align-items-center justify-content-between w-100">
-          <Col md={4}>
+          <Col
+            md={4}
+            className="d-flex align-items-center justify-content-center justify-content-md-start"
+          >
             <Navbar.Brand className="navbar-brand">
-              <Link to="/">
-                <b>SHOPEE ECOMMERCE</b>
+              <Link
+                style={{
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontFamily: 'cursive',
+                  textDecoration: 'none',
+                  marginLeft: '10px',
+                }}
+                to="/"
+              >
+                <b>SHOPEE</b>
               </Link>
             </Navbar.Brand>
           </Col>
 
-          <Col md={5} lg={5}>
+          <Col md={4} className="">
             <Route render={({ history }) => <Search history={history} />} />
           </Col>
 
-          <Col md={3} className="">
+          <Col
+            md={4}
+            className=" d-flex align-items-center justify-content-center justify-content-md-end "
+          >
             <Link to="/cart" style={{ textDecoration: 'none' }}>
               <span id="cart" className="ml-3">
                 Cart
@@ -61,13 +74,13 @@ const Header = () => {
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
-                  <figure className="avatar avatar-nav">
-                    <img
+                  <Figure className="avatar avatar-nav">
+                    <Image
                       src={user.avatar && user.avatar.url}
                       alt={user && user.name}
                       className="rounded-circle"
                     />
-                  </figure>
+                  </Figure>
                   <span>{user && String(user.name).split(' ')[0]}</span>
                 </Link>
 
--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import { InputGroup, Form, Button } from 'react-bootstrap';
 const Search = ({ history }) => {
+  const [keyword, setKeyword] = useState('');
 
-    const [keyword, setKeyword] = useState('');
+  const searchHandler = (e) => {
+    e.preventDefault();
 
-    const searchHandler = (e) => {
-        e.preventDefault()
-
-        if (keyword.trim()) {
-            history.push(`/search/${keyword}`)
-        } else {
-            history.push('/')
-        }
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push('/');
     }
+  };
 
-    return (
-        <form onSubmit={searchHandler} >
-            <div className="input-group">
-                <input
-                    type="text"
-                    id="search_field"
-                    className="form-control"
-                    placeholder="Enter Product Name ..."
-                    onChange={(e) => setKeyword(e.target.value)}
-                />
-                <div className="input-group-append">
-                    <button id="search_btn" className="btn">
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    )
-}
+  return (
+    <Form onSubmit={searchHandler}>
+      <InputGroup>
+        <Form.Control
+          aria-label="Example text with button addon"
+          aria-describedby="basic-addon1"
+          type="text"
+          id="search_field"
+          placeholder="Enter Product Name ..."
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <div className="input-group-append">
+          <Button
+            id="button-addon1"
+            type="submit"
+            style={{
+              height: '40px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            Search
+          </Button>
+        </div>
+      </InputGroup>
+    </Form>
+  );
+};
 
-export default Search
+export default Search;
